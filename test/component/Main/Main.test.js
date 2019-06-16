1129c84d9ff08fbdac1465c08bc020ae4a2d647c
GQSM <ms314006@hotmail.com>
2019-06-16 15:16:06 +0800

    [CHANGE] uninstall react-testing-library, install @testing-library/react

--- test/component/Main/Main.test.js
+++ test/component/Main/Main.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup } from '@testing-library/react';
 import Main from '../../../src/component/Main';
 
 describe('<Main />', () => {
